6e2d9065d6ff9489789dbe597450ae1abc69dc48
unknown <ajpbarry@gmail.com>
2020-01-27 11:42:43 -0600

    added the enumberator, it is working, also started to layour for my tostring methods named twochains

--- MyCustomCollection/Collection.cs
+++ MyCustomCollection/Collection.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace MyCustomCollection
 {
-    public class Collection<T>
+    public class Collection<T> : IEnumerable
     {
         //Member Variables (HAS A)
 
@@ -161,6 +162,10 @@ namespace MyCustomCollection
                 }
 
             }
+        }
+
+        public IEnumerator GetEnumerator()
+        {jgf000
         }
     }
 }
